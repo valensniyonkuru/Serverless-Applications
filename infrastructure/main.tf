1b241d8bf6040604e8a5ea8a37074c7baa95fb28
NIYONKURU VALENS <valensniyonkuru118@gmail.com>
2026-03-02 15:58:14 +0200

    changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws= {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
   required_version = ">= 1.14.2"
@@ -11,47 +11,47 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
- module "dynamodb" {
-   source = "./modules/dynamodb"
+module "dynamodb" {
+  source = "./modules/dynamodb"
 
- }
- module "sns" {
-   source = "./modules/sns"
- }
- module "iam" {
-   source               = "./modules/iam"
-   sns_topic_arn        = module.sns.sns_topic_arn
-   dynamotable_arn      = module.dynamodb.dynamo_table_arn
-   cognito_user_pool_arn = module.cognito.user_pool_arn
- }
+}
+module "sns" {
+  source = "./modules/sns"
+}
+module "iam" {
+  source                = "./modules/iam"
+  sns_topic_arn         = module.sns.sns_topic_arn
+  dynamotable_arn       = module.dynamodb.dynamo_table_arn
+  cognito_user_pool_arn = module.cognito.user_pool_arn
+}
 
 module "cognito" {
-  source = "./modules/cognito"
+  source                = "./modules/cognito"
   pre_signup_lambda_arn = module.lambda.pre_signup_lambda_arn
 }
 
 module "lambda" {
-  source               = "./modules/lambda"
-  cognito_user_pool_arn = module.cognito.user_pool_arn
-  lambda_role_arn       = module.iam.lambda_role_arn
-  task_table_name      = module.dynamodb.dynamo_table_name
-  api_gateway_id       = module.api_gateway.api_id
-  authorizer_id        = module.api_gateway.authorizer_id
+  source                    = "./modules/lambda"
+  cognito_user_pool_arn     = module.cognito.user_pool_arn
+  lambda_role_arn           = module.iam.lambda_role_arn
+  task_table_name           = module.dynamodb.dynamo_table_name
+  api_gateway_id            = module.api_gateway.api_id
+  authorizer_id             = module.api_gateway.authorizer_id
   api_gateway_execution_arn = module.api_gateway.api_gateway_execution_arn
-  user_pool_id         = module.cognito.user_pool_id
+  user_pool_id              = module.cognito.user_pool_id
 }
 
 module "api_gateway" {
-  source = "./modules/api-gateway"
-  user_pool_id = module.cognito.user_pool_id
+  source        = "./modules/api-gateway"
+  user_pool_id  = module.cognito.user_pool_id
   app_client_id = module.cognito.app_client_id
 }
 
-module "amplify"{
-  source = "./modules/amplify"
-  github_token = var.github_token
+module "amplify" {
+  source         = "./modules/amplify"
+  github_token   = var.github_token
   repository_url = var.repository_url
-  api_base_url = module.api_gateway.api_endpoint
-  user_pool_id = module.cognito.user_pool_id
-  app_client_id = module.cognito.app_client_id
+  api_base_url   = module.api_gateway.api_endpoint
+  user_pool_id   = module.cognito.user_pool_id
+  app_client_id  = module.cognito.app_client_id
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "repository_url" {
   description = "URL of the GitHub repository"
   type        = string
-  default = "https://github.com/valensniyonkuru/Serverless-Applications.git"
+  default     = "https://github.com/valensniyonkuru/Serverless-Applications.git"
 }
 variable "github_token" {
   description = "GitHub token for authentication"
